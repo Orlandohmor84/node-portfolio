412c97b8686f2ac60010285eb7a62ab4a42d7f09
SFL-DSO-Orlando <orlando@smartforlife.com>
2023-03-28 17:48:02 -0400

    TIOF View.

--- routes/portfolio-tiof.js
+++ routes/portfolio-tiof.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('portfolio-tiof', 
-    { title: 'iBuildWebs v2',
-      pageId: 'portfolio'
+    { title: 'The Institute of F.I.L.M.S.',
+      link_external: 'https:\/\/theinstituteoffilms.org/home/',
+      pageId: 'portfolio-tiof'
   });
 });
 
