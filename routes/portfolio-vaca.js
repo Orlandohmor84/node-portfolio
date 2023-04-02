257b0591d40117aa9e24a1f6e12a45f2cf41648c
SFL-DSO-Orlando <orlando@smartforlife.com>
2023-04-02 14:05:52 -0400

    Vaca View.

--- routes/portfolio-vaca.js
+++ routes/portfolio-vaca.js
@@ -5,7 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('portfolio-vaca', 
     { title: 'Vaca Exclusive',
-      pageId: 'portfolio'
+      pageId: 'portfolio',
+      summary_p1: 'Designed and developed progressive web app for a vacation sales company using agile, iteration based, just-in-time approach. Connected to Solve CRM via API.',
+      summary_p2: 'Custom scripts were written to deliver user experience based on each unique user\'s browsing activity. Heavy automation for development processes using Gulp such as resizing and compressing images for optimal use in a multiple locations.'
   });
 });
 
