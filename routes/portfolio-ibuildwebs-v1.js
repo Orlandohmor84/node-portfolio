50246071c21f12df9727773de2d7d73a26d65039
SFL-DSO-Orlando <orlando@smartforlife.com>
2023-04-02 12:41:56 -0400

    Webs v1 View.

--- routes/portfolio-ibuildwebs-v1.js
+++ routes/portfolio-ibuildwebs-v1.js
@@ -4,8 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('portfolio-ibuildwebs-v1', 
-    { title: 'iBuildWebs v2',
-      pageId: 'portfolio'
+    { title: 'iBuildWebs v1',
+      pageId: 'portfolio',
+      summary_p1: 'Portfolio website showcasing skills, projects, and development efforts created multiple times using various programming languages and libraries.',
+      summary_p2: 'Exact website was replicated four times using four disticnt ways to build including Angular, jQuery, PHP, and W3.JS.'
   });
 });
 
