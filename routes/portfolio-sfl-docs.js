3977bc9f4ebf958f3be8488756349e7b9d27e586
SFL-DSO-Orlando <orlando@smartforlife.com>
2023-04-07 12:01:54 -0400

    Nav Update.

--- routes/portfolio-sfl-docs.js
+++ routes/portfolio-sfl-docs.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('portfolio-sfl-docs', 
-    { title: 'iBuildWebs v2',
-      pageId: 'portfolio'
+    { title: 'Smart for Life Website Documentation',
+      pageId: 'sfl-docs',
+      summary_p1: 'Website documentation for Smart for Life eCommerce websites to supports users and developers.'
   });
 });
 
