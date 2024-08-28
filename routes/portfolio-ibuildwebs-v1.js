93856be5ad1b8a6a92e57b5ddb7329a304e4bc3f
SFL-DSO-Orlando <orlando@smartforlife.com>
2024-08-28 11:58:20 -0400

    Hid Webs v1 External Links.

--- routes/portfolio-ibuildwebs-v1.js
+++ routes/portfolio-ibuildwebs-v1.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
     { title: 'iBuildWebs v1',
       pageId: 'portfolio',
       summary_p1: 'Portfolio website showcasing skills, projects, and development efforts created multiple times using various programming languages and libraries.',
-      summary_p2: 'Exact website was replicated four times using four disticnt ways to build including Angular, jQuery, PHP, and W3.JS.',
-      link: 'http://ibuildweb.site/'
+      summary_p2: 'Exact website was replicated four times using four distinct ways to build including Angular, jQuery, PHP, and W3.JS.',
+      link: 'https://v1.ibuildwebs.online/'
   });
 });
 
